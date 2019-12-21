703439ba9fa44847cf9bff624b87f5840892b858
Taylor Osbourne <taylorgosbourne@gmail.com>
2019-12-21 16:59:23 -0500

    added inquirer to allow for deleting after chekcing

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import {Command, flags} from '@oclif/command'
 import * as shell from 'shelljs'
 import * as chalk from 'chalk'
 
+const inquirer = require('inquirer')
+
 class NodeCleanup extends Command {
-  static description = 'describe the command here'
+  static description = chalk.green('\n view the amount of storage taken up by node_modules in your project directories and have the option to remove them in one command')
 
   static flags = {
     version: flags.version({char: 'v'}),
@@ -12,20 +14,55 @@ class NodeCleanup extends Command {
     force: flags.boolean({char: 'f'}),
   }
 
-  static args = [{name: 'file'}]
-
   async run() {
     const {flags} = this.parse(NodeCleanup)
 
     const {clean} = flags
 
+    type Answers = {
+      theme: string
+    }
+
+    const cleanUpModules = () => {
+      console.log('\n ')
+      shell.exec('pwd')
+      console.log('\n ')
+      shell.exec('find . -name "node_modules" -type d -prune -print -exec rm -rf "{}" \+')
+      console.log('\n ')
+      console.log(chalk.green('Alrighty then all clean! 🚀'))
+      console.log('\n ')
+    }
+
     if (!clean) {
-      shell.exec(`pwd && find . -name "node_modules" -type d -prune -print | xargs du -chs`)
+      console.log('\n ')
+      console.log(chalk.white.bold('🕵️‍♂️ checking for node_modules in:'))
+      shell.exec('pwd')
+      console.log('\n ')
+      shell.exec('find . -name "node_modules" -type d -prune -print | xargs du -chs')
+      console.log('\n ')
+      inquirer
+        .prompt([
+          {
+            type: 'list',
+            name: 'theme',
+            message: 'Do you want to cleanup (delete) these node_modules?',
+            choices: [
+              'Yes, delete them please',
+              'No, not now'
+            ]
+          }
+        ])
+        .then((answers: Answers) => {
+          const { theme } = answers;
+          if (theme === 'Yes, delete them please') {
+            cleanUpModules()
+          } else {
+            console.log(chalk.green('\n Okay then bye, bye!'))
+            return
+          }
+        })
     } else {
-      shell.exec("pwd && find . -name 'node_modules' -type d -prune -print -exec rm -rf '{}' \+")
-      console.log(chalk.green(`
-Alrighty then all clean! 🚀
-      `))
+      cleanUpModules()
     }
   }
 }
